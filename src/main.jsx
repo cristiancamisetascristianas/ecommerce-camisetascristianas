bdad13d6c38ac01bb8a195842fb423007ff777d2
Daniel Muñoz <70294543+dmunoz05@users.noreply.github.com>
2026-06-27 14:36:59 -0500

    Add version check before app bootstrap
    Refactor main.jsx to include version check before rendering the app.

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { checkAppVersion } from './lib/versionCheck.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+async function bootstrap() {
+  await checkAppVersion()
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+}
+
+bootstrap()
