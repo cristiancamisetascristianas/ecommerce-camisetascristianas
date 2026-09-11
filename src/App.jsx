b3a88bdb0fd49796f5d97f0cd1c19148a80a56a9
Daniel Muñoz <70294543+dmunoz05@users.noreply.github.com>
2026-09-11 14:55:28 -0500

    Refactor App component to include CartProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import { Routes, Route } from "react-router-dom";
 import { ProductsProvider } from "./context/ProductsContext";
+import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import WhatsAppButton from "./components/WhatsAppButton";
+import CartDrawer from "./components/CartDrawer";
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import CatalogoPage from "./pages/CatalogoPage";
 import MarcaPage from "./pages/MarcaPage";
 import ContactoPage from "./pages/ContactoPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PagoResultado from "./pages/PagoResultado";
 
 function App() {
   return (
     <ProductsProvider>
-      <ScrollToTop />
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/catalogo" element={<CatalogoPage />} />
-          <Route path="/marca" element={<MarcaPage />} />
-          <Route path="/contacto" element={<ContactoPage />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </main>
-      <Footer />
-      <WhatsAppButton />
+      <CartProvider>
+        <ScrollToTop />
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/catalogo" element={<CatalogoPage />} />
+            <Route path="/marca" element={<MarcaPage />} />
+            <Route path="/contacto" element={<ContactoPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/pago/exito" element={<PagoResultado estado="exito" />} />
+            <Route path="/pago/pendiente" element={<PagoResultado estado="pendiente" />} />
+            <Route path="/pago/error" element={<PagoResultado estado="error" />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+        <WhatsAppButton />
+        <CartDrawer />
+      </CartProvider>
     </ProductsProvider>
   );
 }
